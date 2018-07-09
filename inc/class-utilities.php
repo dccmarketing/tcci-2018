8fca3646994b626bc39478250cf0356b4debe6c3
Chris Wilcoxson <chris@slushman.com>
2018-07-09 15:52:11 -0500

    Remove home-footer sidebar.

--- inc/class-utilities.php
+++ inc/class-utilities.php
@@ -731,16 +731,6 @@ class TCCI_Utilities {
 			'after_title'   => '</h2>',
 		) );
 
-		register_sidebar( array(
-			'name'          => esc_html__( 'Home Footer', 'tcci' ),
-			'id'            => 'home_footer',
-			'description'   => esc_html__( 'Add widgets here.', 'tcci' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		) );
-
 	} // widgets_init()
 
 	/**
